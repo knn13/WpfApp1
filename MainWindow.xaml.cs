f8313122f5a5203ee6ffe43c25f9dee8b4befa9e
student 04 <stk_lum_04@outlook.de>
2022-04-04 14:58:31 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using WpfApp1.Properties;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        Auto[] autos = new Auto[3];
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(17);
+            timer.Start();
+            timer.Tick += Animiere;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                autos[i] = new Auto(Zeichenfläche);
+            }
+            
+        }
+
+        private void Animiere(object sender, EventArgs e)
+        {
+            Zeichenfläche.Children.Clear();
+          //Zeichenfläche.Children.RemoveRange(0, autos.Length);
+            foreach(Auto item in autos)
+            {
+                item.Bewegen(timer.Interval);
+                item.Zeichnen(Zeichenfläche);
+            }
+        }
+    }
+}
---
+++ Properties/Auto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Properties
+{
+    class Auto
+    {
+        public double PositionX { get; private set; }
+        public double PositionY { get; private set; }
+        public double GeschwindigkeitX { get; private set; }
+        public double GeschwindigkeitY { get; private set; }
+
+        private static Random rnd = new Random();
+
+        //Konstruktor
+        public Auto(Canvas Zeichenfläche)
+        {
+            PositionX =400 * rnd.NextDouble();
+            PositionY = 400 * rnd.NextDouble();
+            GeschwindigkeitX = 800 + 40* rnd.NextDouble();
+            GeschwindigkeitY = 800 + 40* rnd.NextDouble();
+        }
+        //Methoden
+        public void Zeichnen(Canvas Zeichenfläche)
+        {
+            Ellipse e = new Ellipse();
+            e.Width = 10;
+            e.Height = 10;
+            e.Fill = Brushes.Aqua;
+            Canvas.SetLeft(e, PositionX);
+            Canvas.SetTop(e, PositionY);
+            Zeichenfläche.Children.Add(e);
+        }
+
+        public void Bewegen(TimeSpan interval)
+        {
+            PositionX += GeschwindigkeitX * interval.TotalMinutes;
+            PositionY += GeschwindigkeitY * interval.TotalMinutes;
+        }
+    }
+}
